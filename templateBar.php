e933309fbd30f4ebeaf9be82fba29e7e8615be18
adelinelui <38158366+adelinelui@users.noreply.github.com>
2018-04-08 18:54:21 +0800

    Update templateBar.php

--- templateBar.php
+++ templateBar.php
@@ -101,23 +101,23 @@ and open the template in the editor.
 
                 </ul>
                 <ul class="nav navbar-nav">
-                    <li id="notifications3"><a href="#" onclick="notifyMe()"><i class="material-icons">notifications</i></a><span class="badge badge-light">1</span></li>
+                    <li id="notifications3"><a href="notification_page.php" onclick="notifyMe()"><i class="material-icons">notifications</i></a><span class="badge badge-light">1</span></li>
                 </ul>
             </div>
         </nav>
 
         <!-- Sidebar -->
         <div class="w3-sidebar w3-bar-block w3-border-right w3-animate-left"style="display:none" id="mySidebar">
-            <button onclick="w3_close()" class="w3-bar-item w3-large">ADELINE LUI</button>
+            <button onclick="w3_close()" class="w3-bar-item w3-large"></button>
 
             <a href="home_page.php" class="w3-bar-item w3-button w3-small w3-border-bottom w3-hover-black">HOME</a>
-            <a href="profile.php" class="w3-bar-item w3-button w3-small w3-border-bottom w3-hover-black">PROFILE</a>
+            <!--<a href="profile.php" class="w3-bar-item w3-button w3-small w3-border-bottom w3-hover-black">PROFILE</a>-->
             <!--<a href="#" class="w3-bar-item w3-button w3-small w3-border-bottom w3-hover-black">PROMOTION</a>-->
             <a href="news.php" class="w3-bar-item w3-button w3-small w3-border-bottom w3-hover-black">NEWS</a>
             <a href="#" class="w3-bar-item w3-button w3-small w3-border-bottom w3-hover-black">HOW TO USE</a>
             <!--<a href="#" class="w3-bar-item w3-button w3-small w3-border-bottom w3-hover-black">SETTINGS</a>-->
             <a href="#" class="w3-bar-item w3-button w3-small w3-border-bottom w3-hover-black">FAQ</a>
-            <a href="index.php" class="w3-bar-item w3-button w3-small w3-border-bottom w3-hover-black">LOGOUT</a>
+            <!--<a href="index.php" class="w3-bar-item w3-button w3-small w3-border-bottom w3-hover-black">LOGOUT</a>-->
         </div>
 
     </body>
