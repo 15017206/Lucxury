a9145ec169b1cc465e46453b1fd86c9bedfe5572
adelinelui <38158366+adelinelui@users.noreply.github.com>
2018-04-09 09:25:10 +0800

    Update product_page.php

--- product_page.php
+++ product_page.php
@@ -269,5 +269,6 @@ and open the template in the editor.
                     </div>
                 </div>-->
       <?php include 'templateBar_bottom.php'; ?>
+      <?php include 'templateBar_bottombottom.php'; ?>
     </body>
 </html>
