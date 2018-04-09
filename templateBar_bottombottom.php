c2666925c82a8f32bca5d8bcf48783e3725a7f69
adelinelui <38158366+adelinelui@users.noreply.github.com>
2018-04-09 10:11:39 +0800

    Update templateBar_bottombottom.php

--- templateBar_bottombottom.php
+++ templateBar_bottombottom.php
@@ -2,7 +2,7 @@
         <nav class="navbar navbar-expand">
             <div class="container-fluid">
                 <ul class="nav navbar-nav">
-                    <a href="www.google.com" class="w3-display-container w3-display-middle"><h6><i>Terms of Service</a></h6></a>
+                    <a href="www.google.com" class="w3-display-container w3-display-middle w3-text-black"><h6><i>Terms of Service</a></h6></a>
             </ul>
         </nav>
 </html>
