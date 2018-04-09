2781c235247e56bd34670c249cb789b1e470514a
15017206 <15017206@myrp.edu.sg>
2018-04-09 20:48:52 +0800

    Merge origin/master
    Conflicts:
    	product_page.php

--- Webservices/doAddItem.php
+++ Webservices/doAddItem.php
@@ -2,7 +2,7 @@
 include 'dbconn.php';
 
 $merchant_id = $_POST["merchant_id"];
-$name = $_POST["name"];
+$name = $_POST["product_name"];
 $price_currency = $_POST["price_currency"];
 $price_amount = $_POST["price_amount"];
 $brand = $_POST["brand"];
@@ -11,8 +11,8 @@ $condition = $_POST["condition"];
 $category = $_POST["category"];
 $item_more_info_url = $_POST["item_more_info_url"];
 
-$query = "INSERT INTO `item` (`name`,`price_currency`,`price_amount`,`brand`,`color`,`condition`,`category`,`item_more_info_url`)" .
-        "VALUES ('$merchant_id','$name','$price_currency','$price_amount','$brand','$color','$condition','$category','$item_more_info_url')";
+$query = "INSERT INTO `item` (`merchant_id`, `name`, `price_currency`, `price_amount`, `brand`, `color`, `condition`, `category`, `item_more_info_url`)"
+        . " VALUES ('$merchant_id', '$name', '$price_currency', '$price_amount', '$brand', '$color', '$condition', '$category', '$item_more_info_url')";
 
 $result = mysqli_query($link, $query);
 
@@ -23,5 +23,4 @@ if ($result) {
 }
 
 mysqli_close($link);
-
 echo json_encode($response);
