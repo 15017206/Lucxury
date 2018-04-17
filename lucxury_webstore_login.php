cd1b519c18c7cec4730dbdc0597bdc07fd8c72aa
adelinelui <38158366+adelinelui@users.noreply.github.com>
2018-04-17 11:24:03 +0800

    Update lucxury_webstore_login.php

--- lucxury_webstore_login.php
+++ lucxury_webstore_login.php
@@ -7,33 +7,89 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title></title>
-        <?php include 'scripts/bootstrap_scripts/bootstrap_scripts.php'; ?>
-        <style>
-            h5#login_title {
-                text-align: center;
+
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+
+        <!--CSS -->
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+        <script type="text/javascript" src="//downloads.mailchimp.com/js/signup-forms/popup/embed.js" data-dojo-config="usePlainJson: true, isDebug: false"></script>
+
+        <script>
+            function showMailing() {
+
+                require(["mojo/signup-forms/Loader"], function (L) {
+                    L.start({"baseUrl": "mc.us12.list-manage.com", "uuid": "5e744d54c978e566fa533d954", "lid": "ceb70c82f3"})
+
+                })
+                document.cookie = "MCEvilPopupClosed=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
             }
+            
+//            $(document).ready(function(){
+//               $("#subscribe1").onclick(function(){
+//                  alert("456");
+//                                  require(["mojo/signup-forms/Loader"], function (L) {
+//                    L.start({"baseUrl": "mc.us12.list-manage.com", "uuid": "5e744d54c978e566fa533d954", "lid": "ceb70c82f3"})
+//
+//                })
+//                document.cookie = "MCEvilPopupClosed=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
+//                  alert("123");
+//               });
+               
+               
+//            });
+        </script>
+
+        <style>
+            body{
+                width: 100%;
+                background-image: url("images/login_page_images/Untitled.png");
+                background-attachment: fixed;
+                background-repeat: no-repeat;
+                background-size: cover;
+            }
+
+            #logininfo {
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%, -50%);
+                color: white;
+            }
+
+/*            ::placeholder {  Chrome, Firefox, Opera, Safari 10.1+ 
+                color: white;
+                opacity: 1;  Firefox 
+            }
+
+            :-ms-input-placeholder {  Internet Explorer 10-11 
+                color: white;
+            }*/
         </style>
     </head>
     <body>
-        <br/>
-        <div class="container">
-            <div class="alert alert-primary" role="alert">
-                <h5 id='login_title'>Merchant's login to Lucxury</h5>
-
-                <form>
-                    <div class="form-group">
-                        <label for="exampleInputEmail1">Username</label>
-                        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-                        <small id="emailHelp" class="form-text text-muted"></small>
+        <div id="logininfo">
+            <!--<h1 style="font-size: 8vw;">L U C ⚔ U R Y</h1>-->
+            <img src="images/login_page_images/Lucxury Website (Logo) 1.png" alt="" style="max-width: 80%;"/>
+            <h3 style="word-wrap: normal; font-size: 3vw;"><b>MERCHANT'S LOGIN PORTAL</b></h3>
+            <br/>
+            <form>
+                <input class="w3-input w3-text-white w3-small" type="text" placeholder="USERNAME" style="background: transparent">
+                <br/>
+                <input class="w3-input w3-text-white w3-small" type="text" placeholder="PASSWORD" style="background: transparent">
+                <a href="" style="font-size: 80%; text-decoration:none">FORGOT PASSWORD?</a>
+                <br/>
+                <br/>
+                <br/>
+                <div class="w3-row">
+                    <div class="w3-half w3-container">
+                        <a role="button" href="home_page.php" class="w3-block w3-btn w3-white" style="opacity: 0.8;">LOGIN</a>
                     </div>
-                    <div class="form-group">
-                        <label for="exampleInputPassword1">Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-                    </div>
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                </form>
-            </div>
+                </div>
         </div>
-    </body>
+    </form>
+</div>
+</body>
 </html>
