8e57734c912e76b7803313d634c92c52ac3be447
adelinelui <38158366+adelinelui@users.noreply.github.com>
2018-04-23 10:18:00 +0800

    Update templateBar_bottom.php

--- templateBar_bottom.php
+++ templateBar_bottom.php
@@ -6,13 +6,14 @@
                     <div class="w3-display-container w3-display-topmiddle w3-border-top w3-border-bottom w3-border-white">
                         <a href="https://www.facebook.com/lucxuryglobal/?ref=your_pages"><img id="" style="" src="images/templatebar_bottom_images/facebook.jpg" alt=""/></a>
                         
-                        <a href="https://twitter.com/lucxuryglobal"><img id="" style="" src="images/templatebar_bottom_images/twitter.jpg" alt=""/></a>
+                        <a href="https://www.instagram.com/lucxuryglobal/"><img id="" style="" src="images/templatebar_bottom_images/instagram.jpg" alt=""/></a>
                      
-                        <a href="https://www.pinterest.com/lucxury/"><img id="" style="" src="images/templatebar_bottom_images/pinterest.jpg" alt=""/></a>
+                        <!--<a href="https://www.pinterest.com/lucxury/"><img id="" style="" src="images/templatebar_bottom_images/pinterest.jpg" alt=""/></a>-->
                      
                         <a href="https://www.youtube.com/channel/UCeGiwGkYZ9p8kxj8YwN4fIw?view_as=subscriber"><img id="" style="" src="images/templatebar_bottom_images/youtube.jpg" alt=""/></a>
                       
-                        <a href="https://www.instagram.com/lucxuryglobal/"><img id="" style="" src="images/templatebar_bottom_images/instagram.jpg" alt=""/></a>
+                        
+                        <a href="https://twitter.com/lucxuryglobal"><img id="" style="" src="images/templatebar_bottom_images/twitter.jpg" alt=""/></a>
                         </br>
                     </div>
             </ul>
