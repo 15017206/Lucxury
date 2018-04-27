0f64acdeee60147eddc80850af8014040cf3fef3
adelinelui <38158366+adelinelui@users.noreply.github.com>
2018-04-27 09:55:20 +0800

    Add files via upload

---
+++ launchingsoon.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title></title>
+
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+
+        <!--CSS -->
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+        <script type="text/javascript" src="//downloads.mailchimp.com/js/signup-forms/popup/embed.js" data-dojo-config="usePlainJson: true, isDebug: false"></script>
+
+        <script>
+            function showMailing() {
+
+                require(["mojo/signup-forms/Loader"], function (L) {
+                    L.start({"baseUrl": "mc.us12.list-manage.com", "uuid": "5e744d54c978e566fa533d954", "lid": "ceb70c82f3"})
+
+                })
+                document.cookie = "MCEvilPopupClosed=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
+            }
+            
+//            $(document).ready(function(){
+//               $("#subscribe1").onclick(function(){
+//                  alert("456");
+//                                  require(["mojo/signup-forms/Loader"], function (L) {
+//                    L.start({"baseUrl": "mc.us12.list-manage.com", "uuid": "5e744d54c978e566fa533d954", "lid": "ceb70c82f3"})
+//
+//                })
+//                document.cookie = "MCEvilPopupClosed=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
+//                  alert("123");
+//               });
+               
+               
+//            });
+        </script>
+
+        <style>
+            body{
+                width: 100%;
+                background-image: url("images/launchingsoon.jpg");
+                background-attachment: fixed;
+                background-repeat: no-repeat;
+                background-size: cover;
+                
+            }
+
+            #logininfo {
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%, -50%);
+                color: white;
+            }
+
+/*            ::placeholder {  Chrome, Firefox, Opera, Safari 10.1+ 
+                color: white;
+                opacity: 1;  Firefox 
+            }
+
+            :-ms-input-placeholder {  Internet Explorer 10-11 
+                color: white;
+            }*/
+        </style>
+    </head>
+    <body>
+
+</div>
+</body>
+</html>
